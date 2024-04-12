de9746a787c3e72075b5f3e4314140c0ed4fe6b5
Jerry__ <jerryoh45@gmail.com>
2024-04-12 13:53:42 +0700

    Initial Commit: develop branch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  console.log(req.originalUrl);
+  res.set({
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Headers": "content-type, Authorization",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
+    "Access-Control-Max-Age": 86400,
+  });
+  next();
+});
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
