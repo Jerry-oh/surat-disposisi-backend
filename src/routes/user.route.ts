c20da22710f6138d705dc9a01bdbc73ae3e9bdbb
Christian-Tiovanto <christiantiovanto1@gmail.com>
2024-07-06 13:45:15 +0700

    feat(API): adding new API for getting list of all users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import { listUsers, login, register } from "../controllers/user.controller";
+import {
+  getAllUsers,
+  listUsers,
+  login,
+  register,
+} from "../controllers/user.controller";
 import { verifyToken } from "../middleware/auth.middleware";
 import { verifySuperAdmin } from "../middleware/superadmin.middleware";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.get("/list-all-users", verifyToken, getAllUsers);
 router.post("/register", verifyToken, verifySuperAdmin, register);
 router.get("/list-users", verifyToken, verifySuperAdmin, listUsers);
-
 export default router;
